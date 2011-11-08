5fab5f7f179a3a65c0dd40969e1e581bbc8f4508
Dave Simons <dave@inuits.be>
2011-11-08 10:19:10 +0100

    add jobdefs.d subdir
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/jobdefs.pp
+++ manifests/classes/jobdefs.pp
@@ -1,4 +1,12 @@
 class bacula::jobdefs {
+  file {
+    '/etc/bacula/jobdefs.d':
+      ensure => directory,
+      owner => root,
+      group => root,
+      mode => '0640';
+  }
+
   @bacula::jobdefs {
     'DefaultJob':
       name => 'DefaultJob';
