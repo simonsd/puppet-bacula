4b800a5b18f53095d9509ee8e1312461b44b6180
Dave Simons <dave@inuits.be>
2011-11-08 14:57:03 +0100

    add content through concat::fragment rather than content directive
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/definitions/client.pp
+++ manifests/definitions/client.pp
@@ -7,7 +7,13 @@ define bacula::client (
       owner => root,
       group => root,
       mode => 0640,
-      notify => service['bacula-dir'],
-      content => template('bacula/client.erb', 'bacula/job.erb');
+      notify => service['bacula-dir'];
+  }
+
+  concat::fragment {
+    "/etc/bacula/clients.d/${hostname}.conf-client":
+      target => "/etc/bacula/clients.d/${hostname}.conf",
+      content => template('bacula/client.erb'),
+      order => 01;
   }
 }
