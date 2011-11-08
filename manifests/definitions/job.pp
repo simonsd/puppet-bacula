f62b07b81da277c9deb99f981913f67116ec2e16
Dave Simons <dave@inuits.be>
2011-11-08 15:35:28 +0100

    add a name var to job fragments
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/definitions/job.pp
+++ manifests/definitions/job.pp
@@ -3,7 +3,7 @@ define bacula::job (
   $hostname = "${::hostname}"
 ){
   concat::fragment {
-    "/etc/bacula/clients.d/${client}.conf":
+    "/etc/bacula/clients.d/${client}.conf-${name}":
       target => "/etc/bacula/clients.d/${client}.conf",
       content => template('bacula/job.erb'),
       order => '100';
