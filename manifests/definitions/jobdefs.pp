b6ea905edd59b41273af7e5cc44a9f732117bbf5
Dave Simons <dave@inuits.be>
2011-11-15 13:50:28 +0100

    no more default value for storage param in jobdefs def
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/definitions/jobdefs.pp
+++ manifests/definitions/jobdefs.pp
@@ -3,7 +3,7 @@ define bacula::jobdef (
   $level = 'Incremental',
   $fileset = 'FullSet',
   $schedule = 'FullWeeklyIncDaily',
-  $storage = 'File',
+  $storage,
   $messages = 'Standard',
   $pool = 'Default',
   $priority = '10'
