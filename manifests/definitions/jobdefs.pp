8cd27a359194053c823b6a95256b745f0e09e4f6
Dave Simons <dave@inuits.be>
2011-11-15 14:29:23 +0100

    remove storage param from jobdefs def
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/definitions/jobdefs.pp
+++ manifests/definitions/jobdefs.pp
@@ -3,7 +3,6 @@ define bacula::jobdef (
   $level = 'Incremental',
   $fileset = 'FullSet',
   $schedule = 'FullWeeklyIncDaily',
-  $storage = 'File',
   $messages = 'Standard',
   $pool = 'Default',
   $priority = '10'
