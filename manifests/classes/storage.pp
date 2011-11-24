569605d7e5fa71c953b4d930bd94be1b6bc92cfd
Dave Simons <dave@inuits.be>
2011-11-24 16:19:02 +0100

    add concat::setup class to storage class as well
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/storage.pp
+++ manifests/classes/storage.pp
@@ -1,6 +1,7 @@
 # /etc/puppet/modules/bacula/manifests/init.pp
 
 class bacula::storage {
+  include concat::setup
   include bacula::storage::install
   include bacula::storage::config
   include bacula::storage::service
