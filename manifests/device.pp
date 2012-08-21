7dc3a3f4e2be51df4bfdb5a67cb3d0aca5232d0d
Jan Vansteenkiste <jan@vstone.eu>
2012-08-21 20:41:57 +0200

    Fix general formatting a bit of device.pp, fix variable quoting.

--- manifests/device.pp
+++ manifests/device.pp
@@ -1,12 +1,13 @@
 define bacula::device (
   $path
 ){
-  file {
-    "${path}":
-      ensure => directory;
 
-    "/etc/bacula/devices.d/${name}.conf":
-      ensure  => present,
-      content => template('bacula/device.conf.erb');
+  file {$path:
+    ensure => directory,
+  }
+
+  file {"/etc/bacula/devices.d/${name}.conf":
+    ensure  => present,
+    content => template('bacula/device.conf.erb'),
   }
 }
