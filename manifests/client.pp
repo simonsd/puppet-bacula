4b86222f0b1c54bc839d2a5264e6e39df779ea9e
Jan Vansteenkiste <jan@vstone.eu>
2012-08-21 20:41:09 +0200

    Fix variable scope and enclose them in {} where needed in client.pp

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,27 +1,27 @@
 define bacula::client (
   $fqdn,
   $hostname,
-  $storage_server = "${bacula::default_storage_server}.${::domain}",
-  $storage_label = "${bacula::default_storage_label}",
-  $storage_password = "${bacula::default_storage_password}"
+  $storage_server = "${::bacula::default_storage_server}.${::domain}",
+  $storage_label = $::bacula::default_storage_label,
+  $storage_password = $::bacula::default_storage_password
 ) {
   concat {
-    "/etc/bacula/clients.d/${hostname}.conf":
-      owner  => root,
-      group  => root,
-      mode   => 0640,
-      notify => service['bacula-dir'];
+    "/etc/bacula/clients.d/${::hostname}.conf":
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0640',
+      notify => Service['bacula-dir'];
   }
 
   concat::fragment {
-    "/etc/bacula/clients.d/${hostname}.conf-client":
-      target  => "/etc/bacula/clients.d/${hostname}.conf",
+    "/etc/bacula/clients.d/${::hostname}.conf-client":
+      target  => "/etc/bacula/clients.d/${::hostname}.conf",
       content => template('bacula/client.erb'),
       order   => 01;
   }
 
   @@bacula::device {
-    "${hostname}":
-      path => "/mnt/backup/${hostname}";
+    $::hostname:
+      path => "/mnt/backup/${::hostname}";
   }
 }
