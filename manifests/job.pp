d6be30602e5972d12a75d7ba996d45941bd848e6
Jan Vansteenkiste <jan@vstone.eu>
2012-08-21 20:45:26 +0200

    Move comments to the top and added a todo to job.pp

--- manifests/job.pp
+++ manifests/job.pp
@@ -1,18 +1,26 @@
+# == Developers notes:
+#
+#   do not rename the bschedule param to schedule, it'll conflict with the
+#   schedule param for the file resource in the concat module
+#
+# == Todo:
+#
+# TODO: Check variable scope of hostname. Maybe we should use sth else.
+#
+#
 define bacula::job (
   $client,
-  $hostname = "${::hostname}",
-  $fileset = false,
-  $storage = "${hostname}-storage",
-  $full_pool = "${hostname}FullPool",
-  $incremental_pool = "${hostname}IncPool",
-# do not rename this to schedule, it'll conflict with the schedule param
-# for the file resource in the concat module ;)
-  $bschedule = false,
+  $hostname              = $::hostname,
+  $fileset               = false,
+  $storage               = "${hostname}-storage",
+  $full_pool             = "${hostname}FullPool",
+  $incremental_pool      = "${hostname}IncPool",
+  $bschedule             = false,
   $client_run_before_job = false,
-  $client_run_after_job = false,
+  $client_run_after_job  = false,
   $server_run_before_job = false,
-  $server_run_after_job = false,
-  $jobdefs = 'DefaultJob'
+  $server_run_after_job  = false,
+  $jobdefs               = 'DefaultJob'
 ){
   concat::fragment {
     "/etc/bacula/clients.d/${client}.conf-${name}":
