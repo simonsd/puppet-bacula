2eff32f4fd7f8e0b83aecd537e51cb4c5da3acf7
Jan Vansteenkiste <jan@vstone.eu>
2012-08-21 20:45:58 +0200

    Added todo/warning in jobdef.pp

--- manifests/jobdef.pp
+++ manifests/jobdef.pp
@@ -1,19 +1,23 @@
+# == Todo:
+#
+# TODO: warning: schedule is a metaparam; this value will inherit to all
+#       contained resources
+#
 define bacula::jobdef (
   $name,
-  $level = 'Incremental',
-  $fileset = 'FullSet',
+  $level    = 'Incremental',
+  $fileset  = 'FullSet',
   $schedule = 'FullWeekly',
   $messages = 'Standard',
-  $pool = 'Default',
+  $pool     = 'Default',
   $priority = '10'
 ){
-  file {
-    "/etc/bacula/jobdefs.d/${name}.conf":
-      ensure  => 'present',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0640',
-      notify  => Service['bacula-dir'],
-      content => template('bacula/jobdefs.erb');
+  file {"/etc/bacula/jobdefs.d/${name}.conf":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0640',
+    notify  => Service['bacula-dir'],
+    content => template('bacula/jobdefs.erb');
   }
 }
