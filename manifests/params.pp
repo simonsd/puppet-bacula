77fdab3c64e8712be41d7633a3b0ff73a519fe2c
Dave Simons <dave@inuits.eu>
2012-10-24 15:35:49 +0200

    set default values in params.pp
    Signed-off-by: Dave Simons <dave@inuits.eu>

---
+++ manifests/params.pp
@@ -0,0 +1,72 @@
+class bacula::params {
+
+  $director    = 'no'
+  $storage     = 'no'
+  $client      = 'yes'
+
+  $working_dir = $::osfamily ? {
+    /(?i:redhat)/ => '/var/spool/bacula',
+    /(?i:debian)/ => '/var/lib/bacula',
+  }
+
+  ########################
+  ####    Defaults    ####
+  ########################
+
+  $default_storage_server   = 'bacula'
+  $default_storage_label    = 'bacula-sd'
+  $default_storage_password = 'bacula'
+  $default_storage_owner    = 'bacula'
+  $default_storage_group    = 'bacula'
+
+  ########################
+  ####    Database    ####
+  ########################
+
+  $dbname     = 'bacula'
+  $dbuser     = 'bacula'
+  $dbpassword = 'bacula'
+  $dbhost     = 'localhost'
+
+  ########################
+  ####    Director    ####
+  ########################
+
+  $admin_email       = undef
+  $operator_email    = undef
+  $director_server   = 'bacula'
+  $director_label    = 'bacula-dir'
+  $director_password = 'bacula'
+
+  $schedules  = [
+    'FullDaily',
+    'FullWeekly',
+    'IncDaily',
+    'IncWeekly',
+    'DiffDaily',
+    'DiffWeekly',
+    'FullMonthlyDiffWeeklyIncDaily',
+    'FullWeeklyDiffDailyIncHourly',
+    'FullWeeklyIncDaily',
+  ]
+
+  $filesets   = [
+    'Catalog',
+    'FullSet',
+    'MysqlData',
+    'Config',
+  ]
+
+  $jobdefs    = [
+    'DefaultJob',
+    'Mysql',
+    'Config',
+  ]
+
+  #######################
+  ####    Storage    ####
+  #######################
+
+  $storage_dir = '/mnt/backup'
+
+}
