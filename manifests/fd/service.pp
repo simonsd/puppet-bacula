76e8893e6aa064635cdccdb83fc2f23568c0ff2d
Dave Simons <dave@inuits.eu>
2012-10-30 11:13:10 +0100

    fd: clean up syntax
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/fd/service.pp
+++ manifests/fd/service.pp
@@ -1,10 +1,14 @@
 class bacula::fd::service {
 
   service{'bacula-fd':
-    ensure    => running,
+    ensure    => 'running',
     enable    => true,
     subscribe => File["${::bacula::config_root}/bacula-fd.conf"],
-    require   => File["${::bacula::config_root}/bacula-fd.conf", '/var/lib/bacula', '/var/run/bacula'],
+    require   => File[
+      "${::bacula::config_root}/bacula-fd.conf",
+      '/var/lib/bacula',
+      '/var/run/bacula',
+    ],
   }
 
 }
