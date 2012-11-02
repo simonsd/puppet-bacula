a600ebf14c0f2c9b88cac1cdf024fd9dc0079780
Dave Simons <dave@inuits.eu>
2012-11-02 15:59:14 +0100

    fix variables
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/fd.pp
+++ manifests/fd.pp
@@ -1,13 +1,13 @@
 class bacula::fd (
   $client                    = $::fqdn,
-  $fd_port                   = $::bacula::params::fd_port,
+  $port                      = $::bacula::params::fd_port,
   $working_dir               = $::bacula::params::working_dir,
   $pid_dir                   = $::bacula::params::pid_dir,
   $max_concurrent_jobs       = $::bacula::params::fd_max_concurrent_jobs,
   $allowed_director_label    = $::bacula::params::director_label,
   $allowed_director_password = $::bacula::params::director_password,
   $log_output                = $::bacula::params::default_log_output,
-  $fd_pkgname                = $::bacula::params::fd_pkgname,
+  $pkgname                   = $::bacula::params::fd_pkgname,
   $user                      = $::bacula::params::default_user,
   $group                     = $::bacula::params::default_group,
 ) {
