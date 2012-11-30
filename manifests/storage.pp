a4b4f907c8af88636324281edef4a78883355cab
Dave Simons <dave@inuits.eu>
2012-11-30 11:49:46 +0100

    add params to bacula::storage class
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/storage.pp
+++ manifests/storage.pp
@@ -1,6 +1,17 @@
-class bacula::storage {
+class bacula::storage (
+  $sd_label            = $::bacula::params::sd_label,
+  $sd_port             = $::bacula::params::sd_port,
+  $config_dir          = $::bacula::params::config_dir,
+  $working_dir         = $::bacula::params::working_dir,
+  $pid_dir             = $::bacula::params::pid_dir,
+  $max_concurrent_jobs = $::bacula::params::sd_max_concurrent_jobs,
+  $director_label      = $::bacula::params::director_label,
+  $director_password   = $::bacula::params::director_password,
+  $log_output          = $::bacula::params::log_output,
+) inherits ::bacula::params {
 
   include ::concat::setup
+  include ::bacula::console
   include ::bacula::storage::install
   include ::bacula::storage::config
   include ::bacula::storage::service
