338c286de82d5957cb475bf23faefdda9a84a51e
Dave Simons <dave@inuits.eu>
2012-11-30 11:50:37 +0100

    add spacing to params in init.pp for clarity
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,34 @@
 class bacula (
   $admin_email              = $::bacula::params::admin_email,
   $operator_email           = $::bacula::params::operator_email,
+
   $director_server          = $::bacula::params::director_server,
   $director_label           = $::bacula::params::director_label,
   $director_password        = $::bacula::params::director_password,
+
   $default_storage_server   = $::bacula::params::director_password,
   $default_storage_label    = $::bacula::params::default_storage_label,
   $default_storage_password = $::bacula::params::default_storage_password,
   $default_storage_path     = $::bacula::params::default_storage_path,
   $default_storage_owner    = $::bacula::params::default_storage_owner,
   $default_storage_group    = $::bacula::params::default_storage_group,
+
   $storage_dir              = $::bacula::params::storage_dir,
   $working_dir              = $::bacula::params::working_dir,
+  $config_root              = $::bacula::params::config_root,
+
   $dbname                   = $::bacula::params::dbname,
   $dbuser                   = $::bacula::params::dbuser,
   $dbpassword               = $::bacula::params::dbpassword,
   $dbhost                   = $::bacula::params::dbhost,
+
   $director                 = $::bacula::params::director,
   $storage                  = $::bacula::params::storage,
+
   $schedules                = $::bacula::params::schedules,
   $filesets                 = $::bacula::params::filesets,
   $jobdefs                  = $::bacula::params::jobdefs,
-  $config_root              = $::bacula::params::config_root,
+
 ) inherits ::bacula::params {
 
   if $director {
