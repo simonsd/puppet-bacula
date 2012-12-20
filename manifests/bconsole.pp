6e1007ca2076e96c42ab45448238f8baffeb1b1e
Dave Simons <dave@inuits.eu>
2012-12-20 14:17:03 +0100

    fixup bconsole vars
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/bconsole.pp
+++ manifests/bconsole.pp
@@ -4,12 +4,12 @@ class bacula::bconsole {
     ensure => 'present',
   }
 
-  file{"${::bacula::config_root}/bconsole.conf":
+  file{"${config_root}/bconsole.conf":
     owner   => 'root',
     group   => 'root',
     mode    => '0640',
     content => template('bacula/bconsole.conf.erb'),
-    require => Package['bacula-console'],
+    require => Package[$bconsole_pkgname],
   }
 
 }
