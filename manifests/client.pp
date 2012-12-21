80fa9b513051779ec4158ba4d9a5220547b0b3f0
Dave Simons <dave@inuits.eu>
2012-12-21 14:11:36 +0100

    add storage resource to client setup
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/client.pp
+++ manifests/client.pp
@@ -5,7 +5,9 @@ define bacula::client (
   $device_owner     = $::bacula::params::default_device_owner,
   $device_group     = $::bacula::params::default_device_group,
   $fd_port          = $::bacula::params::fd_port,
+  $sd_server        = $::bacula::params::sd_server,
   $sd_port          = $::bacula::params::sd_port,
+  $sd_password      = $::bacula::params::sd_password,
   $config_root      = $::bacula::params::config_root,
 ) {
 
@@ -21,10 +23,18 @@ define bacula::client (
     order   => 01,
   }
 
+  @@bacula::storage{$title:
+    config_root => $config_root,
+    sd_server   => $sd_server,
+    sd_port     => $sd_port,
+    sd_password => $sd_password,
+  }
+
   @@bacula::device{$title:
-    path  => "${storage_path}/${title}",
-    owner => $device_owner,
-    group => $device_group,
+    config_root => $config_root,
+    path        => "${storage_path}/${title}",
+    owner       => $device_owner,
+    group       => $device_group,
   }
 
 }
