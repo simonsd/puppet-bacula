de0df4e1b0a5804f7cb5ecfb38f59ff9be6468fb
Dave Simons <dave@inuits.eu>
2013-01-04 15:09:55 +0100

    remove params with default values from default restore job
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/default/jobs.pp
+++ manifests/default/jobs.pp
@@ -3,8 +3,6 @@ class bacula::default::jobs {
   @@bacula::job{'restore':
     jobtype  => 'Restore',
     fileset  => 'FullSet',
-    storage  => $::bacula::default_storage_server,
-    pool     => 'Full',
   }
 
 }
