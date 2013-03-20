2a4b6cc12776960732d65d1d96bdd9945a6b779b
Dave Simons <dave@inuits.eu>
2013-03-20 13:45:11 +0100

    params: make variables into params
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,52 +1,36 @@
-class bacula::params {
-
-  ######################
-  ####    Common    ####
-  ######################
-
-  $config_root = '/etc/bacula'
-  $working_dir = $::osfamily ? {
+class bacula::params (
+  $config_root                = '/etc/bacula',
+  $working_dir                = $::osfamily ? {
     /(?i:redhat)/ => '/var/spool/bacula',
     /(?i:debian)/ => '/var/lib/bacula',
-  }
-  $pid_dir     = '/var/run/bacula'
-  $log_dir     = '/var/log/bacula'
-
-  ########################
-  ####    Database    ####
-  ########################
-
-  $dbuser     = 'bacula'
-  $dbpassword = 'bacula'
-  $dbhost     = 'localhost'
-
-  ########################
-  ####    Director    ####
-  ########################
-
-  $from_email                 = undef
-  $log_email                  = undef
-  $director_server            = 'bacula'
-  $director_password          = 'bacula'
-  $director_port              = '9101'
-  $director_pkgname           = 'bacula-director-mysql'
+  },
+  $pid_dir                    = '/var/run/bacula',
+  $log_dir                    = '/var/log/bacula',
+  $dbuser                     = 'bacula',
+  $dbpassword                 = 'bacula',
+  $dbhost                     = 'localhost',
+  $from_email                 = undef,
+  $log_email                  = undef,
+  $director_server            = 'bacula',
+  $director_password          = 'bacula',
+  $director_port              = '9101',
+  $director_pkgname           = 'bacula-director-mysql',
   $director_service_name      = $::operatingsystem ? {
     centos => 'bacula-dir',
     debian => 'bacula-director',
-  }
+  },
   $director_service_hasstatus = $::operatingsystem ? {
     default => undef,
     debian  => false,
-  }
-  $director_service_ensure    = 'running'
-  $director_service_enable    = true
+  },
+  $director_service_ensure    = 'running',
+  $director_service_enable    = true,
   $db_init_command            = $::operatingsystem ? {
     centos => '/usr/libexec/bacula/make_bacula_tables',
     debian => '/usr/share/bacula-director/make_mysql_tables',
-  }
-  $dir_max_concurrent_jobs    = '1'
-
-  $schedules  = [
+  },
+  $dir_max_concurrent_jobs    = '1',
+  $schedules                  = [
     'FullDaily',
     'FullWeekly',
     'IncDaily',
@@ -56,84 +40,55 @@ class bacula::params {
     'FullMonthlyDiffWeeklyIncDaily',
     'FullWeeklyDiffDailyIncHourly',
     'FullWeeklyIncDaily',
-  ]
-
-  $filesets   = [
+  ],
+  $filesets                   = [
     'Catalog',
     'FullSet',
     'MysqlData',
     'Config',
-  ]
-
-  $jobdefs    = [
+  ],
+  $jobdefs                    = [
     'DefaultJob',
     'Mysql',
     'Config',
-  ]
-
-  #######################
-  ####    Storage    ####
-  #######################
-
-  $sd_port                = '9103'
-  $storage_dir            = '/mnt/backup'
-  $sd_pkgname             = $::operatingsystem ? {
+  ],
+  $sd_port                    = '9103',
+  $storage_dir                = '/mnt/backup',
+  $sd_pkgname                 = $::operatingsystem ? {
     default => 'bacula-storage-mysql',
     debian  => 'bacula-sd-mysql',
-  }
-  $sd_service_name        = 'bacula-sd'
-  $sd_service_ensure      = 'running'
-  $sd_service_enable      = true
-  $sd_service_hasstatus   = $::operatingsystem ? {
+  },
+  $sd_service_name            = 'bacula-sd',
+  $sd_service_ensure          = 'running',
+  $sd_service_enable          = true,
+  $sd_service_hasstatus       = $::operatingsystem ? {
     default => undef,
     debian  => false,
-  }
-
-  $sd_server              = "bacula.${::domain}"
-  $sd_label               = 'bacula-sd'
-  $sd_password            = 'bacula'
-  $sd_owner               = 'bacula'
-  $sd_group               = 'bacula'
-  $sd_max_concurrent_jobs = '1'
-
-  $media_type             = 'File'
-
-  ###########################
-  ####    File Daemon    ####
-  ###########################
-
-  $fd_port                = '9102'
-  $fd_password            = 'bacula'
-  $fd_max_concurrent_jobs = '1'
-  $fd_pkgname             = 'bacula-client'
-  $fd_service_name        = 'bacula-fd'
-  $fd_service_ensure      = 'running'
-  $fd_service_enable      = true
-  $fd_service_hasstatus   = true
-
-  ########################
-  ####    Bconsole    ####
-  ########################
-
-  $bconsole_pkgname = 'bacula-console'
-
-  ######################
-  ####    Client    ####
-  ######################
-
-  $catalog = 'DefaultCatalog'
-
-  ########################
-  ####    Defaults    ####
-  ########################
-
-  $default_pool            = 'FullPool'
-  $default_jobtype         = 'Backup'
-  $default_jobdefs         = 'DefaultJob'
-  $default_storage         = "${fqdn}-storage"
-  $default_fullpool        = 'FullPool'
-  $default_incrementalpool = 'IncrementalPool'
-  $default_fileset         = 'FullSet'
-  $default_schedule        = 'FullWeekly'
-
+  },
+  $sd_server                  = "bacula.${::domain}",
+  $sd_label                   = 'bacula-sd',
+  $sd_password                = 'bacula',
+  $sd_owner                   = 'bacula',
+  $sd_group                   = 'bacula',
+  $sd_max_concurrent_jobs     = '1',
+  $media_type                 = 'File',
+  $fd_port                    = '9102',
+  $fd_password                = 'bacula',
+  $fd_max_concurrent_jobs     = '1',
+  $fd_pkgname                 = 'bacula-client',
+  $fd_service_name            = 'bacula-fd',
+  $fd_service_ensure          = 'running',
+  $fd_service_enable          = true,
+  $fd_service_hasstatus       = true,
+  $bconsole_pkgname           = 'bacula-console',
+  $catalog                    = 'DefaultCatalog',
+  $default_pool               = 'FullPool',
+  $default_jobtype            = 'Backup',
+  $default_jobdefs            = 'DefaultJob',
+  $default_storage            = "${fqdn}-storage",
+  $default_fullpool           = 'FullPool',
+  $default_incrementalpool    = 'IncrementalPool',
+  $default_fileset            = 'FullSet',
+  $default_schedule           = 'FullWeekly',
+) {
 }
