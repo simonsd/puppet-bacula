42fc288136b67d493713777e506b257000765b28
Dave Simons <dave@inuits.eu>
2013-03-25 12:42:05 +0100

    messages: also pass log file through messages param
    Signed-off-by: Dave Simons <dave@inuits.eu>

--- manifests/messages.pp
+++ manifests/messages.pp
@@ -2,7 +2,7 @@ define bacula::messages (
   $config_root     = $::bacula::params::config_root,
   $log_dir         = $::bacula::params::log_dir,
   $log_file        = '',
-  $messages        = ['append', 'all'],
+  $messages        = '',
   $mailcommand     = $::bacula::params::messages_mailcommand,
   $operatorcommand = $::bacula::params::messages_operatorcommand,
 ) {
@@ -14,12 +14,19 @@ define bacula::messages (
     default => $log_file,
   }
 
-  file{$_log_file:
-    ensure => 'present',
+  $_messages = $messages ? {
+    ""      => ['append', $_log_file, 'all'],
+    default => $messages,
+  }
+
+  File{
+    ensure => 'file',
     mode   => '0644',
     owner  => 'root',
     group  => 'root',
   }
+
+  file{$_log_file:}
 
   file{"${config_root}/messages.d/${name}.conf":
     content => template('bacula/messages.conf.erb'),
